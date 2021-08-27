0d911431bc162411056914e511011ab3e9beab14
sweetygoel <60649407+sweetygoel@users.noreply.github.com>
2021-08-27 18:52:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
